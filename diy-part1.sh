67443c4584e2d17cfd032870bb383a097b0dc435
qxwo <qxwo@qq.com>
2024-10-17 09:30:29 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,7 +17,7 @@
 # echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
 # echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-echo "src-git cups https://github.com/apple/cups.git" >> feeds.conf.default
+echo "src-git cups https://github.com/OpenPrinting/cups.git" >> feeds.conf.default
 # git clone https://github.com/sbwml/luci-app-alist package/alist
 git clone https://github.com/rufengsuixing/luci-app-onliner package/onliner
 #git clone https://github.com/Mythologyli/Xp-58-Tool.git package/Xp-58-Tool
